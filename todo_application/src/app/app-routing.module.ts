013fff4d466f3a2e06268a73e315f1c14b7972f1
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-03-30 00:17:30 +0530

    folder structure and navbar designing done

---
+++ todo_application/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TodoComponent } from './todo/todo.component';
+
+const routes: Routes = [
+
+{path : "", component : HomePageComponent},
+{path : "todo", component : TodoComponent}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
