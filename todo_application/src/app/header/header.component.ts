2d05e981f0977ee5be62fb65a7f4f17070c2ad87
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-03-31 20:01:48 +0530

    multilangual functionality implemented

--- todo_application/src/app/header/header.component.ts
+++ todo_application/src/app/header/header.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+
+  lang = ""
+  constructor(private translateService : TranslateService) {
+       
+  }
+
+  ngOnInit(): void {
+     this.lang = localStorage.getItem("lang") || "en"
+  }
+
+
+  ChangeLang(Lang : any) {
+    // console.log(Lang.target.value)
+
+     const selLang = Lang.target.value
+
+      localStorage.setItem("lang", selLang)
+       
+      this.translateService.use(selLang)
+
+  }
+
+
 
 }
