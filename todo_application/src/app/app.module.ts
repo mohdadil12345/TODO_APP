04c32d685a149b46964078b1c84e83f5a15617d5
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-04-01 20:04:15 +0530

    main

--- todo_application/src/app/app.module.ts
+++ todo_application/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -36,6 +38,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     HttpClientModule,
     CKEditorModule,
     BrowserAnimationsModule,
+    MatIconModule,
     ToastrModule.forRoot(),
     TranslateModule.forRoot({
       loader: {
--- todo_application/src/app/todo/todo.component.ts
+++ todo_application/src/app/todo/todo.component.ts
@@ -7,7 +7,6 @@ import { TranslateService } from '@ngx-translate/core';
 import { catchError, forkJoin } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
 
-
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -28,27 +27,26 @@ export class TodoComponent {
   loading = true;
 
   globalData: any = [];
-  status_fil : string = ""
+  status_fil: string = '';
 
-  descriptionvalue : string = ""
-  titlevalue : string = ""
+  descriptionvalue: string = '';
+  titlevalue: string = '';
 
-  constructor(private http: HttpClient, private translate : TranslateService, private toastr: ToastrService) {}
+  constructor(
+    private http: HttpClient,
+    private translate: TranslateService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
-    
     this.translate.setDefaultLang('en');
-    this.translate.use(localStorage.getItem("lang") || "en")
-
-
+    this.translate.use(localStorage.getItem('lang') || 'en');
 
     this.fetchTodo();
     window.addEventListener('online', () => {
       this.syn_with_server();
     });
   }
-
-
 
   handle_form(ele: NgForm) {
     let obj = {
@@ -57,32 +55,26 @@ export class TodoComponent {
       status: this.status,
       id: this.selected_todo ? this.selected_todo.id : Date.now(),
     };
-    console.log("obj", obj)
+    console.log('obj', obj);
 
     if (navigator.onLine) {
-   
       if (!this.selected_todo) {
         // Add new task
         this.http.post(`${this.url}posts.json`, obj).subscribe(() => {
           this.fetchTodo();
-    this.toastr.success("Todo added Succcefully in online mode ")
-
+          this.toastr.success('Todo added Succcefully in online mode ');
+          this.titlevalue = '';
+          this.descriptionvalue = '';
+          this.status = '';
         });
       } else {
-     
         this.http
           .put(`${this.url}posts/${this.selected_todo._id}.json`, obj)
           .subscribe(() => {
             this.fetchTodo();
           });
       }
-    }
-    
-
-
-    
-    else {
-      
+    } else {
       const lsdata = JSON.parse(localStorage.getItem('todo') || '[]');
       if (this.selected_todo) {
         // Update existing task
@@ -97,9 +89,10 @@ export class TodoComponent {
         // Add new task
         lsdata.push(obj);
         localStorage.setItem('todo', JSON.stringify(lsdata));
-    this.toastr.success("Todo added Succcefully in offline mode ")
-
-        
+        this.toastr.success('Todo added Succcefully in offline mode ');
+        this.titlevalue = '';
+        this.descriptionvalue = '';
+        this.status = '';
       }
       this.fetchTodo();
     }
@@ -118,7 +111,7 @@ export class TodoComponent {
             ...res[key],
           }));
           this.users = newArr;
-          this.globalData = newArr
+          this.globalData = newArr;
           this.loading = false;
         });
     } else {
@@ -128,41 +121,39 @@ export class TodoComponent {
     }
   }
 
-
-
   handle_delete(item: any) {
     let elID = item._id;
     // console.log(elID)
-  
-    if (navigator.onLine) {
-        this.http.delete(`${this.url}posts/${elID}.json`).subscribe((data:any)=> {
-          //  console.log(data)
-           this.fetchTodo()
-           this.toastr.success("Todo deleted Succcefully in online mode ")
 
-             
-      })
-     
+    if (navigator.onLine) {
+      this.http
+        .delete(`${this.url}posts/${elID}.json`)
+        .subscribe((data: any) => {
+          //  console.log(data)
+          this.fetchTodo();
+          this.toastr.success('Todo deleted Succcefully in online mode ');
+        });
     } else {
       let lsdata = JSON.parse(localStorage.getItem('todo') || '[]');
       lsdata = lsdata.filter((ele: any) => ele.id !== item.id);
       localStorage.setItem('todo', JSON.stringify(lsdata));
       this.users = lsdata;
-      this.toastr.success("Todo deleted Succcefully in offline mode ")
-
+      this.toastr.success('Todo deleted Succcefully in offline mode ');
     }
   }
-  
-  handle_status(item : any) {
-    const updatedStatus = item.status === 'Completed' ? 'Pending' : 'Completed';
-  const updatedItem = { ...item, status: updatedStatus };
-    if (navigator.onLine) {
-      this.http.put(`${this.url}posts/${item._id}.json`, updatedItem).subscribe(() => {
-        this.fetchTodo();
-        this.toastr.success("Todo status updated successfully in online mode");
-      });
-    
 
+  handle_status(item: any) {
+    const updatedStatus = item.status === 'Completed' ? 'Pending' : 'Completed';
+    const updatedItem = { ...item, status: updatedStatus };
+    if (navigator.onLine) {
+      this.http
+        .put(`${this.url}posts/${item._id}.json`, updatedItem)
+        .subscribe(() => {
+          this.fetchTodo();
+          this.toastr.success(
+            'Todo status updated successfully in online mode'
+          );
+        });
     } else {
       // Offline mode
       const lsdata = JSON.parse(localStorage.getItem('todo') || '[]');
@@ -172,21 +163,11 @@ export class TodoComponent {
         }
         return ele;
       });
-      localStorage.setItem('todo', JSON.stringify(updatedData)); 
-      this.users = updatedData; 
-    this.toastr.success("Status changed Successfull in offline mode")
-
+      localStorage.setItem('todo', JSON.stringify(updatedData));
+      this.users = updatedData;
+      this.toastr.success('Status changed Successfull in offline mode');
     }
   }
-  
-
-
-
-
-
-
-
-
 
   edit_popup(ele: TodoTypes) {
     this.selected_todo = ele;
@@ -206,30 +187,23 @@ export class TodoComponent {
     this.show_popup = false;
   }
 
-
-
-
   syn_with_server() {
     const lsdata = JSON.parse(localStorage.getItem('todo') || '[]');
     const syncRequests = lsdata.map((item: any) => {
-        return this.http.post(`${this.url}posts.json`, item);
+      return this.http.post(`${this.url}posts.json`, item);
     });
 
     forkJoin(syncRequests).subscribe(
-        () => {
-            localStorage.removeItem('todo');
-            this.fetchTodo(); 
-        },
-        error => {
-            console.error('Error syncing item:', error);
-           
-        }
+      () => {
+        localStorage.removeItem('todo');
+        this.fetchTodo();
+      },
+      (error) => {
+        console.error('Error syncing item:', error);
+      }
     );
-}
+  }
 
-
-
-  
   toggleCKEditor(descriptionId: any): void {
     Object.keys(this.editorStates).forEach((key) => {
       this.editorStates[key] = false;
@@ -237,35 +211,33 @@ export class TodoComponent {
     this.editorStates[descriptionId] = true;
   }
 
-
-//  sharable link 
+  //  sharable link
 
   shareable_link() {
-      if(navigator.share) {
-         navigator.share({
-          title : "todo-app",
-          url : "https://todo-application-pied-phi.vercel.app/"
-         }).then(()=> {
-          console.log("share success")
-         }).catch((err)=> {
-           alert("something error")
-         })
-      }
+    if (navigator.share) {
+      navigator
+        .share({
+          title: 'todo-app',
+          url: 'https://todo-application-pied-phi.vercel.app/',
+        })
+        .then(() => {
+          console.log('share success');
+        })
+        .catch((err) => {
+          alert('something error');
+        });
+    }
   }
-
-
 
   //  select_lang
 
-  select_lang(event : any){
+  select_lang(event: any) {
     // console.log(event.target.value)
     const selectedLang = event.target.value;
-this.translate.use(selectedLang);
-localStorage.setItem("lang", selectedLang);
-this.toastr.success("Language changed successfully")
-
+    this.translate.use(selectedLang);
+    localStorage.setItem('lang', selectedLang);
+    this.toastr.success('Language changed successfully');
   }
-
 
   //  search by title
 
@@ -275,39 +247,26 @@ this.toastr.success("Language changed successfully")
     this.users = this.globalData.filter((ele: any) =>
       ele.title.toLowerCase().includes(this.serchfilter.toLowerCase())
     );
-
   }
 
-
-//  filter my status
+  //  filter my status
   filter_by_status() {
-   if(this.status_fil == "") {
-    this.users  = this.globalData
-   }else{
-    this.users = this.globalData.filter((ele:any) => 
-         ele.status.includes(this.status_fil)
-      )
-    
-   }
+    if (this.status_fil == '') {
+      this.users = this.globalData;
+    } else {
+      this.users = this.globalData.filter((ele: any) =>
+        ele.status.includes(this.status_fil)
+      );
+    }
   }
 
-
-
-
-  descriptionvalueChange(event : any) {
-    console.log(event)
-    this.descriptionvalue = event
+  descriptionvalueChange(event: any) {
+    console.log(event);
+    this.descriptionvalue = event;
   }
 
-
-  titleChange(event : any){
-    console.log(event)
-    this.titlevalue = event
+  titleChange(event: any) {
+    console.log(event);
+    this.titlevalue = event;
   }
-
-
 }
-
-
-
-
